f4e72bd9fb2b13cc136276a6ec65856ff076e270
docAlexxx <docalex@mail.ru>
2021-11-08 16:12:49 +0400

    добавлены кнопка перехода с mainActivity на SecondActivity и код для кнопки на SecondActivity

--- app/src/main/java/com/example/lesson1homework/MainActivity.java
+++ app/src/main/java/com/example/lesson1homework/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.lesson1homework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Button secondScreenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        secondScreenButton = findViewById(R.id.second_screen_button);
+
+        secondScreenButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/lesson1homework/SecondActivity.java
+++ app/src/main/java/com/example/lesson1homework/SecondActivity.java
@@ -3,12 +3,27 @@ package com.example.lesson1homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private TextView resultText;
+    private Button button;
+    private EditText positionTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
+        resultText = (TextView) findViewById(R.id.resolution);
+        button = findViewById(R.id.done_button);
+        positionTitle = findViewById(R.id.position_title);
+
+        button.setOnClickListener(v -> {
+            String position = positionTitle.getText().toString();
+            resultText.setText("Profile for " + position + " is added.");
+        });
     }
 }
